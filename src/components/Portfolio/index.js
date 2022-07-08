0788958f9475b79334a998bbb59d29f38787763f
Matthew Parker <mattparker124@gmail.com>
2022-07-07 20:27:43 -0500

    added public url tracking

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -8,7 +8,7 @@ const slideData = [
       paragraph: 'An online and offline budget tracking app to help you manage funds on-the-go from anywhere, even without an internet connection, all packaged into a simple PWA that can be installed straight onto your desktop or phone!',
       button: 'Start Budgeting!',
       location: 'https://pacific-island-20775.herokuapp.com/',
-      src: '%PUBLIC_URL%/slides/budget.png'
+      src: '/react-portfolio/slides/budget.png'
     },
     {
       index: 1,
@@ -16,7 +16,7 @@ const slideData = [
       paragraph: 'A social networking app utuilizing NoSQL to allow developers to make an account and discuss various development-related topics.',
       button: 'Get Started!',
       location: 'https://codewireapp.herokuapp.com/',
-      src: '%PUBLIC_URL%/slides/codewire.png'
+      src: '/react-portfolio/slides/codewire.png'
     },
     {
       index: 2,
@@ -24,7 +24,7 @@ const slideData = [
       paragraph: 'Plan out your work-day in realtime in this app that remembers your schedule and tracks it throughout the day.',
       button: 'Get Focused!',
       location: 'https://mattparker124.github.io/work-day-scheduler/',
-      src: '%PUBLIC_URL%/slides/work-day.png'
+      src: '/react-portfolio/slides/work-day.png'
     },
     {
       index: 3,
@@ -32,7 +32,7 @@ const slideData = [
       paragraph: 'Run Buddy was my first project in HTML and CSS, setting up a basic website using the fundamentals of front-end development.',
       button: `Let's Run!`,
       location: 'https://mattparker124.github.io/run-buddy/',
-      src: '%PUBLIC_URL%/slides/run-buddy.png'
+      src: '/react-portfolio/slides/run-buddy.png'
     }
 ]
 

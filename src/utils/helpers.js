c977d8e6375cd5f96f9ce5b1b746a256bb62e45d
Matthew Parker <mattparker124@gmail.com>
2022-07-07 19:46:47 -0500

    added contact form and submission

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,8 @@
 export function openInNewTab(url) {
     window.open(url, '_blank', 'noopener,noreferrer');
 }
+
+export function validateEmail(email) {
+    var re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
